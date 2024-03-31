3bf4f02624c413cce3d2ae9b314426b782f4cde7
Ryan Morris <mryan.morris@gmail.com>
2024-03-31 12:13:04 +0000

    feat(web-client): add a scaffolded web client

---
+++ web-client/src/App.page.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import App from './App';
+
+export class AppPage {
+    render() {
+        render(<App />);
+    }
+
+    get heading(): HTMLElement {
+        return screen.getByRole('heading', { name: 'Game Together Online' });
+    }
+}
---
+++ web-client/tests/app.page.ts
@@ -0,0 +1,16 @@
+import { Locator, Page } from "@playwright/test";
+
+export class AppPage {
+    private readonly page: Page;
+
+    readonly heading: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.heading = this.page.getByRole('heading', { name: 'Game Together Online' });
+    }
+
+    async goto() {
+        await this.page.goto('/');
+    }
+}
